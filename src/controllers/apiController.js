7ddb6e4a44dd1d46fe69c3078dc35b0aa8814d98
Oliver Coley <mxoliverleigh@gmail.com>
2019-04-16 15:39:16 -0400

    update history routes

--- src/controllers/apiController.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-const authClient = require('../config/google-config').oauth2Client;
-
-module.exports = {
-  
-}
