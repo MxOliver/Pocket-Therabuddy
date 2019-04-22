e5db134451ceffdcf87a5c349c1b56292eaeff82
Oliver Coley <mxoliverleigh@gmail.com>
2019-04-22 15:21:55 -0400

    static build files

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -36,7 +36,7 @@ module.exports = {
             app.use(express.static('client/build'));
 
             app.get('*', (req, res) => {
-              res.sendFile(path.resolve(__dirname, '..', '..', '..', 'client', 'build', 'index.html'));
+              res.sendFile(path.join(__dirname+'../../client/build/index.html'));
             })
         }
 
