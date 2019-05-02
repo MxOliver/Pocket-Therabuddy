a3cfc28a27ff353f10f0d20561bf2bc66f89d124
Oliver Coley <mxoliverleigh@gmail.com>
2019-05-02 11:45:59 -0400

    edit graph points

---
+++ client/src/components/mood/MoodChart.jsx
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+
+class MoodChart extends Component {
+
+    render() {
+
+        const data = this.props.data;
+        const loaded = this.props.loaded;
+        
+        let dataSet = [];
+
+        for(let i = 0; i < data.length; i++){
+            Object.values(data[i]).forEach(e => {
+                if(e['moodselect'] === "happy"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'happy'}
+                    )
+                }
+                if(e['moodselect'] === "sad"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'sad' }
+                    )
+                }
+                if(e['moodselect'] === "active"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'active' }
+                    )
+                }
+                if(e['moodselect'] === "anxious"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'anxious' }
+                    )
+                }
+                if(e['moodselect'] === "angry"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'angry' }
+                    )
+                }
+                if(e['moodselect'] === "fine"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'fine' }
+                    )
+                }
+                if(e['moodselect'] === "tired"){
+                    dataSet.push(
+                        { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'tired' }
+                    )
+                }
+            });
+        }
+
+
+        var margin = {top: 50, right: 50, bottom: 50, left: 80},
+        width = 1050 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+        let dataPoints = dataSet.map(function(d) {
+            return {
+                date: new Date(d.date),
+                level: +d.level,
+                mood: d.mood
+            };
+        });
+
+
+        let xScale = d3.scaleTime().domain(d3.extent(dataPoints, function(d) { return d.date})).range([margin.left, width - margin.right]);
+        let yScale = d3.scaleLinear().domain([0, 100]).range([height - margin.bottom, margin.top]);
+
+        let line = d3.line()
+        .x(function(d) { return xScale(d.date)})
+        .y(function (d) { return yScale(d.level)})
+        .curve(d3.curveMonotoneX)
+
+    var svg = d3.select('#moodChart').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var dataNest = d3.nest().key(function(d) { return d.mood; }).entries(dataPoints) 
+        
+    let color = d3.scaleOrdinal(d3.schemePaired);
+
+    let legendSpace = height / dataNest.length;
+
+    svg.append('g').classed('data-points', true);
+
+    dataNest.forEach(function(d, i){
+
+        svg.append('path')
+            .attr('class', 'line')
+            .style('stroke', function() {
+                return d.color = color(d.key);
+            })
+            .style('stroke-width', 3)
+            .attr('d', line(d.values))
+
+
+        svg.append('text')
+            .attr('y', (legendSpace/5)+ i * legendSpace)
+            .attr('x', width - (margin.right / 3) + 5)
+            .attr('class', 'legend')
+            .style('fill', function() {
+                return d.color = color(d.key);
+            })
+            .text(d.key)
+
+    });
+
+    svg.append('g').attr('class', 'xAxis').attr('transform', 'translate(0,' + height + ')')
+    .call(d3.axisBottom(xScale).ticks(d3.timeDay.every(1)).tickFormat(d3.timeFormat('%a %b %d %Y')));
+
+    svg.append('g').attr('class', 'yAxis').call(d3.axisLeft(yScale));
+
+    var points = svg.select('g.data-points').selectAll('dot')
+        .data(dataPoints.filter(function(d) {
+            return d.level;
+        }))
+        .enter()
+
+    points.append('circle')
+        .attr('r', 4)
+        .attr('class', 'circles')
+        .style('fill', function(d) { return d.color = color(d.mood)})
+        .attr('cx', function(d) { return xScale(d.date); })
+        .attr('cy', function(d) { return yScale(+d.level); })
+
+
+    if(!loaded){
+        return <p>Ready when you are...</p>
+    } else {
+        return (
+            <div className="chart" style={{display: 'flex', justifyContent: 'space-around'}}>
+                <div id="moodChart" />
+            </div>
+        );
+    }
+
+    }
+}
+
+
+export default MoodChart;
--- client/src/components/mood/MoodHistory.jsx
+++ client/src/components/mood/MoodHistory.jsx
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import HistoryNav from '../partials/HistoryNav';
 import { moodActions } from '../../actions/moodActions';
 import { MDBBtn, MDBContainer } from 'mdbreact';
-import * as d3 from 'd3';
-import moment from 'moment';
+import MoodChart from './MoodChart';
 
 function mapStateToProps(state){
     const { fetchMoods, authentication } = state;
@@ -22,10 +21,11 @@ class ConnectedMoodHistory extends Component {
         super();
 
         this.state = {
-            loaded: false
+            loaded: false,
+            data: ''
         }
 
-        this.getData = this.getData.bind(this);
+        this.fetchData = this.fetchData.bind(this);
     }
 
     componentDidMount() {
@@ -33,144 +33,17 @@ class ConnectedMoodHistory extends Component {
         dispatch(moodActions.getMoodHistory(user.response));
     }
 
-    getData(){
-    const { moods } = this.props;
-    let temp = JSON.parse(moods);
-    let data = [];
-    Object.keys(temp).forEach((e) => (
-        data.push(temp[e])
-    ));
-    let dataSet = [];
-
-    for(let i = 0; i < data.length; i++){
-        Object.values(data[i]).forEach(e => {
-            if(e['moodselect'] === "happy"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'happy'}
-                )
-            }
-            if(e['moodselect'] === "sad"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'sad' }
-                )
-            }
-            if(e['moodselect'] === "active"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'active' }
-                )
-            }
-            if(e['moodselect'] === "anxious"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'anxious' }
-                )
-            }
-            if(e['moodselect'] === "angry"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'angry' }
-                )
-            }
-            if(e['moodselect'] === "fine"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'fine' }
-                )
-            }
-            if(e['moodselect'] === "tired"){
-                dataSet.push(
-                    { date: new Date(Date.parse(e['createdAt'])), level: e['moodlevel'], mood: 'tired' }
-                )
-            }
-        });
+    fetchData() {
+        const { moods } = this.props;
+        let tempData = JSON.parse(moods);
+        let data = [];
+        Object.keys(tempData).forEach((e) => (
+            data.push(tempData[e])
+        ));
+        this.setState({ data: data, loaded: true });
     }
-
-    var margin = {top: 50, right: 50, bottom: 50, left: 80},
-    width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-    let dataPoints = dataSet.map(function(d) {
-        console.log(d.date)
-            return {
-                date: new Date(d.date),
-                level: +d.level,
-                mood: d.mood
-            };
-    });
-
-    console.log(dataPoints); 
-
-    // const maxDate = moment()
-    // const minDate = moment().subtract(2, 'week');
-
-    let xScale = d3.scaleTime().domain(d3.extent(dataPoints, function(d) { return d.date})).range([margin.left, width - margin.right]);
-    let yScale = d3.scaleLinear().domain([0, 100]).range([height - margin.bottom, margin.top]);
-
-    let line = d3.line()
-        .x(function(d) { return xScale(d.date)})
-        .y(function (d) { return yScale(d.level)})
-        .curve(d3.curveMonotoneX)
-
-    var svg = d3.select('#moodChart').append('svg')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-    var dataNest = d3.nest().key(function(d) { return d.mood; }).entries(dataPoints) 
-        
-    let color = d3.scaleOrdinal(d3.schemePaired);
-
-    let legendSpace = height / dataNest.length;
-
-    dataNest.forEach(function(d, i){
-        svg.append('path')
-            .attr('class', 'line')
-            .style('stroke', function() {
-                return d.color = color(d.key);
-            })
-            .style('stroke-width', 3)
-            .attr('d', line(d.values))
-
-        svg.append('text')
-            .attr('y', (legendSpace/5)+ i * legendSpace)
-            .attr('x', width - (margin.right / 3) + 5)
-            .attr('class', 'legend')
-            .style('fill', function() {
-                return d.color = color(d.key);
-            })
-            .text(d.key)
-
-    });
-
-    svg.append('g').attr('class', 'xAxis').attr('transform', 'translate(0,' + height + ')')
-        .call(d3.axisBottom(xScale).ticks(d3.timeDay.every(1)).tickFormat(d3.timeFormat('%a %b %d %Y')));
-
-    svg.append('g').attr('class', 'yAxis').call(d3.axisLeft(yScale));
-    
-    this.setState({ loaded: true })
-    }
-
 
     render() {
-        const { loaded } = this.state;
-        const { fetched, dateFetched } = this.props;
-        let moodChart = null;
-
-        if(loaded === false){
-            if(fetched === false || dateFetched === false){
-                moodChart = (
-                    <em>Data is loading...</em>
-                )
-            } else {
-                moodChart = (
-                    <em>Ready when you are... </em>
-                )
-            }
-        } else {
-            moodChart = (
-                <div className="charts">
-                    <div id="moodChart" />
-                </div>
-            )
-        }
 
         const navStyle = {
             marginBottom: '35px'
@@ -180,11 +53,11 @@ class ConnectedMoodHistory extends Component {
             <div className="content">
             <HistoryNav style={navStyle} />
             <div className="container">
-            <MDBBtn id="fetchButton" outline color='red lighten-3' onClick={this.getData}>
-            Click Twice to Fetch History
+            <MDBBtn id="fetchButton" outline color='red lighten-3' onClick={this.fetchData}>
+            Click Twice to Render Graph
             </MDBBtn>
            <MDBContainer>
-            {moodChart}
+            <MoodChart data={this.state.data} loaded={this.state.loaded} style={{ paddingLeft: '-50px'}}/>
             </MDBContainer>
             </div>
             </div>
